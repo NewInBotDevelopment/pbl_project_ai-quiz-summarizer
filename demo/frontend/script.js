e1f3cf47bc7fe0d1c7b8017ea4abaa2af6b66ab2
Lakshya Katiyar <72309173+NewInBotDevelopment@users.noreply.github.com>
2026-04-13 14:19:44 +0530

    Update script.js

--- demo/frontend/script.js
+++ demo/frontend/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const BACKEND_URL = "https://lecturai-backend.onrender.com";
+const BACKEND_URL = "http://127.0.0.1:5000";
 let _selectedFile = null;
 
 // ─── THEME ────────────────────────────────────
